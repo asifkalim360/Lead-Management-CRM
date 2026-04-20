55e94906c0669bc31cee9ddb37248939dd9f7f8d
asifkalim360 <asifkalim360@gmail.com>
2026-04-20 15:17:02 +0530

    Added LeadRepository using Spring Data JPA

---
+++ src/main/java/com/crm/repository/LeadRepository.java
@@ -0,0 +1,11 @@
+package com.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crm.entity.Lead;
+
+@Repository
+public interface LeadRepository extends JpaRepository<Lead, Long> {
+
+}
