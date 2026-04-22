32c6617986f49c71d5116df242dfd94793c5b037
asifkalim360 <asifkalim360@gmail.com>
2026-04-22 20:31:33 +0530

    Added LeadController with create and fetch APIs

---
+++ src/main/java/com/crm/controller/LeadController.java
@@ -0,0 +1,39 @@
+package com.crm.controller;
+
+
+import com.crm.dto.LeadRequestDto;
+import com.crm.dto.LeadResponseDto;
+import com.crm.service.LeadService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/leads")
+public class LeadController {
+    private final LeadService leadService;
+
+    public LeadController(LeadService leadService)
+    {
+        this.leadService = leadService;
+    }
+
+    @PostMapping
+    public LeadResponseDto createLead(@Valid @RequestBody LeadRequestDto requestDto)
+    {
+        return leadService.createLead(requestDto);
+    }
+
+    @GetMapping
+    public List<LeadResponseDto> getAllLeads()
+    {
+        return leadService.getAllLeads();
+    }
+
+    @GetMapping
+    public LeadResponseDto getLeadById(@PathVariable Long id)
+    {
+        return leadService.getLeadById(id);
+    }
+}
